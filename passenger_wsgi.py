7fba26cd250353e4fbb5bf1c84958509fde9a111
Elisha-AI-bit <mubmaps7@gmail.com>
2026-02-01 16:21:34 -0500

    helloe

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,9 +1,11 @@
-import imp
 import os
 import sys
 
-
+# path to your app
 sys.path.insert(0, os.path.dirname(__file__))
 
-wsgi = imp.load_source('wsgi', 'passenger_wsgi.py')
-application = wsgi.application
+# change PROJECTNAME to your Django project folder
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bene_safe.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
